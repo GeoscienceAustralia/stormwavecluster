fb18b4db9ee482cc196118261333c7ee9b680505
gareth <gareth@gareth-P410.com>
2023-04-28 09:42:21 +1000

    support for tpx09v5a in tidal prediction interface

--- R/tpxo7.2/BASIC_R_INTERFACE/OTPS_directory_name_TPXO9.R
+++ R/tpxo7.2/BASIC_R_INTERFACE/OTPS_directory_name_TPXO9.R
@@ -1,6 +1,6 @@
 # YOU MUST CHANGE THE NAME OF THIS VARIABLE TO BE THE DIRECTORY CONTAINING THE 'PREDICT_TIDE' PROGRAM
 .OTPS_directory = '/home/gareth/Code_Experiments/TIDAL_PREDICTION/TPXO9/OTPS'
-.TIDAL_MODEL_CONTROL_FILE = 'DATA/Model_tpxo9.v2'
+.TIDAL_MODEL_CONTROL_FILE = 'DATA/Model_tpxo9.v5a'
 
 if(!file.exists(.OTPS_directory)){
     msg = paste0('Cannot file OTPS_directory: ',
